d9f3d071ba287327fe4174de8153f96c732b2745
jokerwrld999 <ifalaleev49@gmail.com>
2023-08-21 22:42:42 +0300

    [refactoring] Inventory Update

--- roles/base/files/users/root/zsh/bindkey.zsh
+++ roles/base/files/users/root/zsh/bindkey.zsh
@@ -1,37 +1,6 @@
 # Set bindkey to emacs mode
 bindkey -e
 
-# Restore history searching with ^r
-bindkey '^r' history-incremental-search-backward
-
-# Use alt and arrow keys for moving directories
-cdUndoKey() {
-  popd      > /dev/null
-  zle       reset-prompt
-  echo
-  ls
-  echo
-}
-
-cdParentKey() {
-  pushd .. > /dev/null
-  zle      reset-prompt
-  echo
-  ls
-  echo
-}
-
-zle -N                 cdParentKey
-zle -N                 cdUndoKey
-bindkey '^[[1;3A'      cdParentKey
-bindkey '^[[1;3D'      cdUndoKey
-
-
-# Control-x-e to open current line in $EDITOR, awesome when writing functions or editing multiline commands.
-autoload -U edit-command-line
-zle -N edit-command-line
-bindkey '^x^e' edit-command-line
-
 # Custom ZSH Binds
 bindkey '^@' autosuggest-accept
 bindkey '^H' backward-kill-word
